f809bc5f2a7688ebc45ca8539d2ce9b40a2ca123
Liviu Bogdan Martinescu <hi@mlb.ro>
2024-08-05 13:09:14 +0300

    docs template

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Klean UI</span>,
+  project: {
+    link: "https://github.com/bogdanmartinescu/Klean-UI",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/bogdanmartinescu/Klean-UI/docs",
+  footer: {
+    text: "ui.mlb.ro",
+  },
+};
+
+export default config;
